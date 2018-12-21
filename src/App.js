2a1daebaa7006455a6aa510e22146b8646664d46
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-21 13:11:07 -0500

    initial commit, ui navbar/dashboard/sidebar setup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AppNavbar from './components/layout/AppNavbar';
+import Dashboard from './components/layout/Dashboard';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router> 
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
